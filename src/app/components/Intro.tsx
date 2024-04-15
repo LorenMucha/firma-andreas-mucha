98798ed1f88d7c7ab16687a94c21fa37cc0e5982
LorenMucha <lorenmucha@gmail.com>
2024-04-15 19:24:03 +0200

    add start page

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -1,4 +1,4 @@
-import { CCarousel, CCarouselItem, CImage } from "@coreui/react"
+import { CImage } from "@coreui/react"
 
 const Leistungen = [
     { image: "/images/bad.jpg", title: "Bad" },
@@ -7,17 +7,26 @@ const Leistungen = [
 ]
 
 const Intro = (): React.JSX.Element =>
-    <div className="w-screen h-screen flex flex-col justify-center items-center bg-white">
-        <CCarousel controls indicators wrap={false}>
-            {Leistungen.map((leistung, index) => (
-                <CCarouselItem key={index} className="w-100 p-5">
-                    <CImage align="center" width={1400} rounded src={leistung.image} alt="slide 1" />
-                    <div className="text-center text-2xl text-black">
-                        {leistung.title}
+    <div className="w-screen h-screen bg-white flex justify-center items-center flex-wrap">
+        <div className="container grid grid-cols-1 gap-3">
+            <div className="text-4xl text-black text-center">Herzlich Wilkommen </div>
+            <div className="flex justify-center items-center">
+                {Leistungen.map((leistung, index) => (
+                    <div key={index}>
+                        {<div className="max-w-sm rounded shadow-lg ml-5 hover:scale-110 ">
+                            <CImage className="w-full" src={leistung.image} />
+                            <div className="px-6 py-4">
+                                <div className="font-bold text-xl mb-2 text-blue-900">{leistung.title}</div>
+                                <p className="text-gray-700 text-base">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                                </p>
+                            </div>
+                        </div>
+                        }
                     </div>
-                </CCarouselItem>
-            ))}
-        </CCarousel>
+                ))}
+            </div>
+        </div>
     </div>
 
 export default Intro
