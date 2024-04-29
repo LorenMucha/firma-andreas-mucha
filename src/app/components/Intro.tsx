381ad224702e29e0769edd03d04683312f7aefd4
LorenMucha <lorenmucha@gmail.com>
2024-04-29 19:05:08 +0200

    rm coreui

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -1,13 +1,19 @@
-import { CImage } from "@coreui/react"
+import Image from "next/image"
+import bad from "../../../public/images/bad/bad_intro.svg"
+import dach from "../../../public/images/dach/dach_intro.svg"
+import hausTechnik from "../../../public/images/haustechnik/haustechnik_intro.svg"
+import heizung from "../../../public/images/heizung/heizung_intro.svg"
 
-const images = ['dach/dach_intro.svg', 'haustechnik/haustechnik_intro.svg', 'heizung/heizung_intro.svg', 'bad/bad_intro.svg']
+const images = [dach, hausTechnik, heizung, bad]
+
+//const images = ['dach/dach_intro.svg', 'haustechnik/haustechnik_intro.svg', 'heizung/heizung_intro.svg', 'bad/bad_intro.svg']
 
 const Intro = (): React.JSX.Element =>
     <div className="sm:w-full sm:h-screen bg-white flex sm:justify-center lg:items-center flex-col px-5">
         <div className="text-2xl pb-2 sm:mt-0 mt-24 w-auto text-left"> Ihr Fachbetrieb für</div>
         <div className="sm:ml-0 ml-4 grid grid-cols-2 gap-1 w-auto h-auto">
             {images.map((image, index) => (
-                <CImage key={index} className="w-full duration-300 ease-in-out hover:scale-110" src={`/images/${image}`} />
+                <Image key={index} className="w-full duration-300 ease-in-out hover:scale-110" src={image} />
             ))}
         </div>
     </div>
